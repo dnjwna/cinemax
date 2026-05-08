7b7a8d599399d56ca64bfaa24c6d900c559e1e34
danujuwana <danujuwana05@gmail.com>
2026-05-08 19:04:55 +0700

    feat: update api baseURL to VPS, add env config

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://38.47.180.195/api",
 });
 
 api.interceptors.request.use((config) => {
